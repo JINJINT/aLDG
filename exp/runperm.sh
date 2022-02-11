f7ffe38aec8895eb5e972d876a7163879e3b8a51
Jinjin Tian <jinjint@andrew.cmu.edu>
2022-02-10 21:25:13 -0500

    update

--- exp/runperm.sh
+++ exp/runperm.sh
@@ -7,11 +7,12 @@ experiment_name="permval"
 #  do
     #for i in 1 2 3 4
     #do
-      for s in $(seq 1 200)
+      #for s in $(seq 1  200)
+      for s in 107 112 114 115 117 118 127 131 134 137 142 148 157 168 182 195 28 63 71 77 85 92
       do
         echo "Permutation test of instance "$i", perm batch = "$s
         command="$Rscript ./exp/perm.sh $s"
-        sbatch --time=00:10:00 -p RM-shared -N 1 -n 1 -J $experiment_name $command
+        sbatch --time=01:00:00 -p RM-shared -N 1 -n 1 -J $experiment_name $command
       done
     #done
 # done
